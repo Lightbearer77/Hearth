f1c91e2c5a71c9c347722ffb96247838c7740f05
Nick <test@outlook.com>
2026-06-27 17:27:46 -0600

    Fix corrupted babel.config.js + add babel-preset-expo

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
-cat > ~/Hearth/babel.config.js << 'EOF'
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
 };
